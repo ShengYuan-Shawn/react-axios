fc8a20adbf487625eb9993923444bc2ed37aec1b
syshawn <lsy2pw@gmail.com>
2022-07-15 00:25:49 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+// Declaring API's URL
+const baseURL = "https://jsonplaceholder.typicode.com/comments";
 
 function App() {
+  const [post, setPost] = React.useState(null);
+  const [newName, setNewName] = React.useState("");
+  const [newEmail, setNewEmail] = React.useState("");
+  const [newBody, setNewBody] = React.useState("");
+
+  // GET Request
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  // POST Request
+  function createPost() {
+    axios
+      .post(baseURL, { title: "Hello World!", body: "This is a new post." })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  // PUT Request
+  function updatePost() {
+    axios
+      .put(`${baseURL}/1`, {
+        title: "Hello World!",
+        body: "This is an updated post.",
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
+
+  // DELETE Request
+  function deletePost() {
+    axios.delete(`${baseURL}/1`).then(() => {
+      alert("Post deleted!");
+      setPost(null);
+    });
+  }
+
+  if (!post) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">API Request</h1>
+      <div className="get-container">
+        <h2 className="title">Get Request</h2>
+      </div>
+      <div className="post-container">
+        <form className="post-form">
+          <label>Name</label>
+          <input></input>
+          <label>E-mail</label>
+          <input></input>
+          <label>Body</label>
+          <input></input>
+          <button onClick={createPost}>Create Post</button>
+        </form>
+      </div>
+      <div className="put-container">
+        <button onClick={updatePost}>Update Post</button>
+      </div>
+      <div>
+        <button onClick={deletePost}>Delete Post</button>
+      </div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 }
